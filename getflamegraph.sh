cdba90dc0dd659663ff1f532bb9ffe61b9c842fb
nzpr <nuttzipper@gmail.com>
2019-11-17 23:27:00 +0300

    Update getflamegraph.sh

--- getflamegraph.sh
+++ getflamegraph.sh
@@ -1,11 +1,11 @@
 # Kill all RNode instances
 kill -9 `jps | grep "Main" | cut -d " " -f 1`
 
-# Clear the state to always run node with fresh genesis
+# Clear the state to always run node with fresh state
 rm -Rf ~/.rnode/tmp/ && rm -Rf ~/.rnode/rspace && \
 rm -Rf ~/.rnode/dagstorage && rm -Rf ~/.rnode/blockstore && \
 rm -Rf ~/.rnode/last-finalized-block && rm -Rf ~/.rnode/rnode.log && \
-tmux new-session -d -s "rnode-benchmark" ./node/target/docker/stage/opt/docker/bin/rnode run -s \
+tmux new-session -d -s "rnode-benchmark" ./node/target/universal/stage/bin/rnode run -s \
 --synchrony-constraint-threshold 0.0 --validator-private-key $(cat ~/.rnode/genesis/*.sk | tail -1)
 
 grep -q "Requested fork tip from peers" ~/.rnode/rnode.log 2> /dev/null; ready=$?
@@ -18,9 +18,9 @@ done
 
 # Deploy contract passed as arg
 echo "RNode ready. Deploying..."
-rnode deploy --phlo-limit 100000000000000 --phlo-price 1 \
+./node/target/universal/stage/bin/rnode deploy --phlo-limit 100000000000000 --phlo-price 1 \
 --private-key f8d756125e35da03cc66d5b8411881e72ea57f65bd67800f650336d1a9f827fb $1 
-echo "Proposing"; rnode propose &
+echo "Proposing"; ./node/target/universal/stage/bin/rnode propose &
 # Sleep for 2s to make sure we're running profiler in the middle of propose, dump flamegraph 
 sleep 2 
 echo "Starting profiler" && ~/async-profiler/profiler.sh -d 30 $(jps | grep Main | cut -d " " -f1) -f $1.svg
